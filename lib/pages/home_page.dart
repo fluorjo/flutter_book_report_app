e21a4ade6bc208f83f2966596088a556d9dcbf66
fluorjo <fluorjo@naver.com>
2022-12-26 07:25:39 +0900

    appbar추가

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,25 @@
-import 'package:book_report/components/logo.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          "Book Report",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          const SizedBox(
-            height: 200,
-          ),
-          const Logo("Book Report"),
-          const SizedBox(
-            height: 30,
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text("Get Started"),
-          ),
-        ]),
+        child: Column(children: const []),
       ),
     );
   }
