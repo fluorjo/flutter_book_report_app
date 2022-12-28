8d886581d57d6d19eeb219f7d3aeace6c7ce00eb
fluorjo <fluorjo@naver.com>
2022-12-28 10:29:11 +0900

    책 정보 불러오기 성공?

--- lib/pages/book_page.dart
+++ lib/pages/book_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+//import 'dart:convert';
 
 class BookPage extends StatelessWidget {
   @override
@@ -30,15 +31,20 @@ class _HttpApp extends State<HttpApp> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var url = 'http://www.google.com';
-          var response = await http.get(Uri.parse(url));
-          setState(() {
-            result = response.body;
-          });
+        onPressed: () {
+          getJSONData();
         },
         child: const Icon(Icons.file_download),
       ),
     );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+//    var response = await http.get(Uri.parse(url));
+    var response = await http.get(Uri.parse(url),
+        headers: {"Authorization": "KakaoAK cc88f89bd0a8b58cce4d732fac88b0e8"});
+    print(response.body);
+    return "성공";
   }
 }
