a89a257c9b03d907a3abb6d738600043da5af072
fluorjo <fluorjo@naver.com>
2023-01-01 15:15:58 +0900

    LaunchScreen

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,42 @@
+import 'package:book_report/pages/login_page.dart';
+import 'event_screen.dart';
+
+import 'package:flutter/material.dart';
+
+import '../shared/authentication.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LaunchScreen extends StatefulWidget {
+  @override
+  State<LaunchScreen> createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp();
+    Authentication auth = Authentication();
+    auth.getUser().then((user) {
+      MaterialPageRoute route;
+if(user!=null){
+  route=MaterialPageRoute(builder: (context)=>EventScreen());
+}else{
+  route=MaterialPageRoute(builder: (context)=>LoginPage());
+}
+
+    }).catchError((err) => print(err));
+  }
+}
