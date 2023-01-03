6c1744129ee69030adc5c8491be200047e37c1b2
fluorjo <fluorjo@naver.com>
2023-01-03 14:09:02 +0900

    즐겨찾기 추가에서 null 문제 해결

--- lib/shared/firestore_helper.dart
+++ lib/shared/firestore_helper.dart
@@ -6,10 +6,12 @@ class FirestoreHelper {
   static final FirebaseFirestore db = FirebaseFirestore.instance;
 
   static Future addFavorite(EventDetail eventDetail, String uid) {
-    Favorite fav = Favorite(null!, eventDetail.id, uid);
-    var result = db.collection('favorites').add(fav.toMap())
+    Favorite fav = Favorite('', eventDetail.id, uid);
+    var result = db
+        .collection('favorites')
+        .add(fav.toMap())
         .then((value) => print(value))
-        .catchError((error)=> print (error));
+        .catchError((error) => print(error));
     return result;
   }
 
